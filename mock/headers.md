acb295e2a0384b11b13b11f6116b90439e6ace18
Septias <scoreplayer2000@gmail.com>
2022-10-29 14:07:50 +0200

    some tests

---
+++ mock/headers.md
@@ -0,0 +1,27 @@
+### issue_close
+Request URL: http://188.68.57.24:8080/receive
+Request method: POST
+Accept: 
+content-type: application/json
+User-Agent: GitHub-Hookshot/ede37db
+X-GitHub-Delivery: 11db8960-576f-11ed-8bee-b9d9206fb4d5
+X-GitHub-Event: issues
+X-GitHub-Hook-ID: 385934421
+X-GitHub-Hook-Installation-Target-ID: 558781383
+X-GitHub-Hook-Installation-Target-Type: repository
+X-Hub-Signature: sha1=e02e1e1f5f4b8efa3c0d084921206ec44e318fd1
+X-Hub-Signature-256: sha256=b13a49c443f9beac6066c2d7b997284b660112132401d0ac26c5c1b9891c7f6c
+
+### issue_open
+Request URL: http://188.68.57.24:8080/
+Request method: POST
+Accept:
+content-type: application/json
+User-Agent: GitHub-Hookshot/ede37db
+X-GitHub-Delivery: c7d2a640-56d4-11ed-9785-9fac91628040
+X-GitHub-Event: issues
+X-GitHub-Hook-ID: 385934421
+X-GitHub-Hook-Installation-Target-ID: 558781383
+X-GitHub-Hook-Installation-Target-Type: repository
+X-Hub-Signature: sha1=d833e79ff21e6c6eb77d9a456fe088499634c1ba
+X-Hub-Signature-256: sha256=6909bcbff0137c9da4b762d713be94e095b8e17deaa0f082c8fb0fa0306c36dc
--- src/server.rs
+++ src/server.rs
@@ -1,5 +1,11 @@
-use std::sync::{Arc, Mutex};
-use web_server::{HttpMethod, Response};
+use anyhow::bail;
+use json::JsonValue;
+use std::{
+    string::ParseError,
+    sync::{Arc, Mutex},
+};
+use thiserror::Error;
+use web_server::{HttpMethod, Request, Response};
 
 struct Server {
     storage: String,
@@ -13,9 +19,23 @@ fn make_str(httpmethod: HttpMethod) -> &'static str {
     }
 }
 
-enum Event {
-    IssueCreated { user: String },
+#[derive(Error, Debug, PartialEq, Eq)]
+enum Error {
+    #[error("Unable to parse Request")]
+    ParseError,
+
+    #[error("Not Covered")]
+    NotCovered,
+    /* #[error(transparent)]
+    Other(#[from] anyhow::Error), */
 }
+
+#[derive(Debug, PartialEq, Eq)]
+enum Event {
+    IssueOpened { user: String },
+    IssueClosed { user: String },
+}
+
 
 impl Server {
     fn new() -> Self {
@@ -38,6 +58,37 @@ impl Server {
     }
 }
 
+fn parse_issue(payload: JsonValue) -> Result<Event, Error> {
+    println!("{:?}", payload["action"]);
+    if let JsonValue::Short(action) = &payload["action"] {
+        match action.as_str() {
+            "closed" => Ok(Event::IssueClosed {
+                user: payload["sender"]["login"].to_string(),
+            }),
+            "opened" => Ok(Event::IssueOpened {
+                user: payload["sender"]["login"].to_string(),
+            }),
+            _ => Err(Error::NotCovered),
+        }
+    } else {
+        Err(Error::ParseError)
+    }
+}
+
+// this should never return an error
+fn try_parse(req_body: &str) -> Result<JsonValue, Error> {
+    json::parse(req_body).or_else(|_| Err(Error::ParseError))
+}
+
+fn receive_webhoook(req: Request) -> Result<Event, Error> {
+    match req.params.get("X-GitHub-Event") {
+        Some(event_type) if event_type == "pull_request" => todo!(),
+        Some(event_type) if event_type == "issue" => parse_issue(try_parse(&req.get_body())?),
+        Some(_) => Err(Error::NotCovered),
+        None => Err(Error::ParseError),
+    }
+}
+
 pub fn start_server() {
     let server = Arc::new(Mutex::new(Server::new()));
     web_server::new()
@@ -50,4 +101,32 @@ pub fn start_server() {
             Box::new(move |a, _| server.lock().unwrap().post(a))
         })
         .launch(8080);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Event;
+
+    use super::*;
+
+    #[test]
+    fn test_issue_closed() {
+        let mock = json::parse(include_str!("../mock/issue_close.json")).unwrap();
+        assert_eq!(
+            parse_issue(mock),
+            Ok(Event::IssueClosed {
+                user: String::from("Septias")
+            })
+        );
+    }
+    #[test]
+    fn test_issue_opened() {
+        let mock = json::parse(include_str!("../mock/issue_open.json")).unwrap();
+        assert_eq!(
+            parse_issue(mock),
+            Ok(Event::IssueOpened {
+                user: String::from("Septias")
+            })
+        );
+    }
 }

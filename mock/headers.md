c81e5838ac84add1ed0c3a386fd5fb808aa70a27
Septias <scoreplayer2000@gmail.com>
2022-10-29 14:49:37 +0200

    rewrite server

--- mock/headers.md
+++ mock/headers.md
@@ -25,3 +25,31 @@ X-GitHub-Hook-Installation-Target-ID: 558781383
 X-GitHub-Hook-Installation-Target-Type: repository
 X-Hub-Signature: sha1=d833e79ff21e6c6eb77d9a456fe088499634c1ba
 X-Hub-Signature-256: sha256=6909bcbff0137c9da4b762d713be94e095b8e17deaa0f082c8fb0fa0306c36dc
+
+### pr_closed
+Request URL: http://188.68.57.24:8080/receive
+Request method: POST
+Accept: 
+content-type: application/json
+User-Agent: GitHub-Hookshot/ede37db
+X-GitHub-Delivery: de1fbda0-576c-11ed-8a09-559842830225
+X-GitHub-Event: pull_request
+X-GitHub-Hook-ID: 385934421
+X-GitHub-Hook-Installation-Target-ID: 558781383
+X-GitHub-Hook-Installation-Target-Type: repository
+X-Hub-Signature: sha1=b9b62c9bc5e5b522904ec420eac729a40acb364e
+X-Hub-Signature-256: sha256=3cdabef03befa9c8bcca4aa95293dab8d9e0f1108d40f81b75e45b6165a6f732
+
+### pr_opened
+Request URL: http://188.68.57.24:8080/receive
+Request method: POST
+Accept: 
+content-type: application/json
+User-Agent: GitHub-Hookshot/ede37db
+X-GitHub-Delivery: 25ec21b0-576c-11ed-83cd-abde7a5e2dc1
+X-GitHub-Event: pull_request
+X-GitHub-Hook-ID: 385934421
+X-GitHub-Hook-Installation-Target-ID: 558781383
+X-GitHub-Hook-Installation-Target-Type: repository
+X-Hub-Signature: sha1=3b6ebb42773afabd6344ddda2f0ee0f45d3f240e
+X-Hub-Signature-256: sha256=ea796226bccffbeff32aed33300bac9d610e318d1e6b21783b9db4ef0f40b19d
--- src/server.rs
+++ src/server.rs
@@ -1,11 +1,13 @@
-use anyhow::bail;
-use json::JsonValue;
+use anyhow::{anyhow, bail};
+use serde::{Deserialize, Serialize};
 use std::{
     string::ParseError,
     sync::{Arc, Mutex},
 };
 use thiserror::Error;
 use web_server::{HttpMethod, Request, Response};
+
+use self::{issue::IssueEvent, pr::PREvent};
 
 struct Server {
     storage: String,
@@ -19,114 +21,171 @@ fn make_str(httpmethod: HttpMethod) -> &'static str {
     }
 }
 
-#[derive(Error, Debug, PartialEq, Eq)]
+#[derive(Error, Debug)]
 enum Error {
     #[error("Unable to parse Request")]
-    ParseError,
+    ParseError(#[from] serde_json::Error),
 
     #[error("Not Covered")]
     NotCovered,
-    /* #[error(transparent)]
-    Other(#[from] anyhow::Error), */
+
+    #[error(transparent)]
+    Other(#[from] anyhow::Error),
 }
 
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
+pub struct User {
+    pub login: String,
+}
+
+#[derive(Debug)]
 enum Event {
-    IssueOpened { user: String },
-    IssueClosed { user: String },
+    Issue(IssueEvent),
+    PR(PREvent),
 }
+mod issue {
+    use serde::{Deserialize, Serialize};
 
+    use super::User;
 
-impl Server {
-    fn new() -> Self {
-        Self {
-            storage: String::new(),
-        }
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    #[serde(rename_all = "lowercase")]
+    pub enum IssueAction {
+        Opened,
+        Edited,
+        Deleted,
+        Pinned,
+        Unpinned,
+        Closed,
+        Reopened,
+        Assigned,
+        Unassigned,
+        Labeled,
+        Unlabeled,
+        Locked,
+        Unlocked,
+        Transferred,
+        Milestoned,
+        Demilestoned,
     }
-    fn get(&mut self, request: web_server::Request) -> Response {
-        self.storage.clone().into()
-    }
-
-    fn post(&mut self, request: web_server::Request) -> Response {
-        let t = &format!(
-            "< {}: {}>\n",
-            make_str(request.get_method()),
-            request.get_body()
-        );
-        self.storage += t;
-        "Ok".into()
-    }
-}
-
-fn parse_issue(payload: JsonValue) -> Result<Event, Error> {
-    println!("{:?}", payload["action"]);
-    if let JsonValue::Short(action) = &payload["action"] {
-        match action.as_str() {
-            "closed" => Ok(Event::IssueClosed {
-                user: payload["sender"]["login"].to_string(),
-            }),
-            "opened" => Ok(Event::IssueOpened {
-                user: payload["sender"]["login"].to_string(),
-            }),
-            _ => Err(Error::NotCovered),
-        }
-    } else {
-        Err(Error::ParseError)
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    pub struct IssueEvent {
+        pub action: IssueAction,
+        pub sender: User,
     }
 }
 
-// this should never return an error
-fn try_parse(req_body: &str) -> Result<JsonValue, Error> {
-    json::parse(req_body).or_else(|_| Err(Error::ParseError))
+mod pr {
+    use serde::{Deserialize, Serialize};
+
+    use super::User;
+
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    #[serde(rename_all = "lowercase")]
+    pub enum PRAction {
+        Opened,
+        Edited,
+        Closed,
+        Reopened,
+        Assigned,
+        Unassigned,
+        ReviewRequested,
+        ReviewRequestRemoved,
+        Labeled,
+        Unlabeled,
+        Synchronized,
+    }
+    #[derive(Serialize, Deserialize, Debug, PartialEq)]
+    pub struct PREvent {
+        pub action: PRAction,
+        pub sender: User,
+    }
 }
 
 fn receive_webhoook(req: Request) -> Result<Event, Error> {
     match req.params.get("X-GitHub-Event") {
-        Some(event_type) if event_type == "pull_request" => todo!(),
-        Some(event_type) if event_type == "issue" => parse_issue(try_parse(&req.get_body())?),
+        Some(event_type) if event_type == "pull_request" => Ok(Event::Issue(
+            serde_json::from_str::<IssueEvent>(&req.get_body())?,
+        )),
+        Some(event_type) if event_type == "issue" => {
+            Ok(Event::PR(serde_json::from_str::<PREvent>(&req.get_body())?))
+        }
         Some(_) => Err(Error::NotCovered),
-        None => Err(Error::ParseError),
+        None => Err(Error::Other(anyhow!("Missing header `X-GitHub-Event`"))),
     }
 }
 
 pub fn start_server() {
-    let server = Arc::new(Mutex::new(Server::new()));
     web_server::new()
-        .get("/", {
-            let server = server.clone();
-            Box::new(move |a, _| server.lock().unwrap().get(a))
-        })
-        .post("/receive", {
-            let server = server.clone();
-            Box::new(move |a, _| server.lock().unwrap().post(a))
-        })
+        .post(
+            "/receive",
+            Box::new(|req, resp| {
+                receive_webhoook(req);
+                resp
+            }),
+        )
         .launch(8080);
 }
 
 #[cfg(test)]
 mod tests {
-    use super::Event;
+    use crate::server::{
+        issue::{IssueAction, IssueEvent},
+        pr::{PRAction, PREvent},
+        User,
+    };
 
-    use super::*;
+    use super::Event;
 
     #[test]
     fn test_issue_closed() {
-        let mock = json::parse(include_str!("../mock/issue_close.json")).unwrap();
+        let mock = include_str!("../mock/issue_close.json");
         assert_eq!(
-            parse_issue(mock),
-            Ok(Event::IssueClosed {
-                user: String::from("Septias")
-            })
+            serde_json::from_str::<IssueEvent>(mock).unwrap(),
+            IssueEvent {
+                action: IssueAction::Closed,
+                sender: User {
+                    login: "Septias".to_owned()
+                }
+            }
         );
     }
     #[test]
     fn test_issue_opened() {
-        let mock = json::parse(include_str!("../mock/issue_open.json")).unwrap();
+        let mock = include_str!("../mock/issue_open.json");
         assert_eq!(
-            parse_issue(mock),
-            Ok(Event::IssueOpened {
-                user: String::from("Septias")
-            })
+            serde_json::from_str::<IssueEvent>(mock).unwrap(),
+            IssueEvent {
+                action: IssueAction::Opened,
+                sender: User {
+                    login: "Septias".to_owned()
+                }
+            }
+        );
+    }
+    fn test_pr_closed() {
+        let mock = include_str!("../mock/pr_closed.json");
+        assert_eq!(
+            serde_json::from_str::<PREvent>(mock).unwrap(),
+            PREvent {
+                action: PRAction::Closed,
+                sender: User {
+                    login: "Septias".to_owned()
+                }
+            }
+        );
+    }
+    #[test]
+    fn test_pr_opened() {
+        let mock = include_str!("../mock/pr_opened.json");
+        assert_eq!(
+            serde_json::from_str::<PREvent>(mock).unwrap(),
+            PREvent {
+                action: PRAction::Opened,
+                sender: User {
+                    login: "Septias".to_owned()
+                }
+            }
         );
     }
 }
